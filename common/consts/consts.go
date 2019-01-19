e7494c9aa81976e37ab38158eaef860a4a34c1ce
mdcman <mediacoin@mediacoin.pro>
2019-01-20 01:04:51 +0300

    add consts

---
+++ common/consts/consts.go
@@ -0,0 +1,11 @@
+package consts
+
+const (
+	Byte int64 = 1
+	KiB  int64 = 1 << 10
+	MiB  int64 = 1 << 20
+	GiB  int64 = 1 << 30
+	TiB  int64 = 1 << 40
+	PiB  int64 = 1 << 50
+	EiB  int64 = 1 << 60
+)
