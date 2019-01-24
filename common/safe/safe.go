af14cb10a544d13850e4a9f21fc588b819874132
mdcman <mediacoin@mediacoin.pro>
2019-01-24 10:52:26 +0300

    add safe pack

---
+++ common/safe/safe.go
@@ -0,0 +1,40 @@
+package safe
+
+import (
+	"runtime/debug"
+	"time"
+
+	"github.com/mediacoin-pro/core/common/xlog"
+)
+
+func Exec(fn func()) {
+	defer func() { recover() }()
+	defer Recover()
+	fn()
+}
+
+func Loop(duration time.Duration, fn func()) {
+	for {
+		Exec(fn)
+		time.Sleep(duration)
+	}
+}
+
+func Recover() {
+	defer func() { recover() }()
+	if r := recover(); r != nil {
+		ss := debug.Stack()
+		xlog.Fatal.Print("Panic:\n", string(ss))
+		xlog.Fatal.Printf("FATAL-ERR: %v", r)
+	}
+}
+
+func RecoverAndReport() {
+	defer func() { recover() }()
+	if r := recover(); r != nil {
+		xlog.Fatal.Print("Panic:\n", string(debug.Stack()))
+		//SendReport(fnName, r)
+		xlog.Fatal.Printf("FATAL-ERR: %v", r)
+		panic(r)
+	}
+}
