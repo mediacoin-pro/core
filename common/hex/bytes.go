40e53754f4447385886c3f47d54aaf4f9d17c8c2
mdcman <mediacoin@mediacoin.pro>
2019-02-05 20:27:25 +0300

    hex objects

---
+++ common/hex/bytes.go
@@ -0,0 +1,37 @@
+package hex
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"io"
+
+	"github.com/mediacoin-pro/core/common/bin"
+)
+
+type Bytes []byte
+
+func (b Bytes) String() string {
+	return hex.EncodeToString(b)
+}
+
+func (b Bytes) BinaryEncode(w io.Writer) error {
+	return bin.NewWriter(w).WriteBytes([]byte(b))
+}
+
+func (b *Bytes) BinaryDecode(r io.Reader) (err error) {
+	*b, err = bin.NewReader(r).ReadBytes()
+	return
+}
+
+func (b Bytes) MarshalJSON() ([]byte, error) {
+	return []byte(`"` + b.String() + `"`), nil
+}
+
+func (b *Bytes) UnmarshalJSON(data []byte) (err error) {
+	var s string
+	if err = json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	*b, err = hex.DecodeString(s)
+	return
+}
---
+++ common/hex/uint64.go
@@ -0,0 +1,59 @@
+package hex
+
+import (
+	"encoding/json"
+	"io"
+	"strconv"
+	"strings"
+
+	"github.com/mediacoin-pro/core/common/bin"
+)
+
+type Uint64 uint64
+
+func (i Uint64) Uint64() uint64 {
+	return uint64(i)
+}
+
+func (i Uint64) String() string {
+	return EncodeUint(uint64(i))
+}
+
+func (i Uint64) BinaryEncode(w io.Writer) error {
+	return bin.NewWriter(w).WriteVarUint64(uint64(i))
+}
+
+func (i Uint64) BinaryDecode(r io.Reader) (err error) {
+	u, err := bin.NewReader(r).ReadVarUint64()
+	i = Uint64(u)
+	return
+}
+
+func (i Uint64) MarshalJSON() ([]byte, error) {
+	if i == 0 {
+		return []byte(`""`), nil
+	}
+	return []byte(`"` + i.String() + `"`), nil
+}
+
+func (i *Uint64) UnmarshalJSON(data []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+	switch v := v.(type) {
+	case float64:
+		*i = Uint64(v)
+	case string:
+		if v == "" {
+			return nil
+		}
+		v = strings.TrimPrefix(v, "0x")
+		u64, err := strconv.ParseUint(v, 16, 64)
+		if err != nil {
+			return err
+		}
+		*i = Uint64(u64)
+	}
+	return nil
+}
