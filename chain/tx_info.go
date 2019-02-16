2a07c9fd18e392a05c4df8bf6549d84b2c7a0032
mdcman <mediacoin@mediacoin.pro>
2019-02-16 18:34:07 +0300

    tx-info,json

---
+++ chain/tx_info.go
@@ -0,0 +1,57 @@
+package chain
+
+import "github.com/mediacoin-pro/core/common/bin"
+
+type TxInfo struct {
+	*Transaction
+}
+
+func (t TxInfo) IsNil() bool {
+	return t.Transaction == nil
+}
+
+func (t TxInfo) Encode() []byte {
+	if t.Transaction == nil {
+		return bin.Encode(
+			0,
+			0,
+			0,
+			0,
+			0,
+		)
+	}
+
+	// caching tx-users info (see: Transaction.UsernameByID())
+	t.Transaction.MarshalJSON()
+
+	return bin.Encode(
+		t.Transaction,
+		t.blockNum,
+		t.blockIdx,
+		t.blockTs,
+		t._users,
+	)
+}
+
+func (t *TxInfo) Decode(data []byte) (err error) {
+	var (
+		blockNum uint64 //
+		blockIdx int    //
+		blockTs  int64  //
+		users    map[uint64]string
+	)
+	err = bin.Decode(data,
+		&t.Transaction,
+		&blockNum,
+		&blockIdx,
+		&blockTs,
+		&users,
+	)
+	if t.Transaction != nil {
+		t.Transaction.blockNum = blockNum
+		t.Transaction.blockIdx = blockIdx
+		t.Transaction.blockTs = blockTs
+		t.Transaction._users = users
+	}
+	return
+}
---
+++ chain/tx_json.go
@@ -0,0 +1,59 @@
+package chain
+
+import (
+	"encoding/json"
+
+	"github.com/mediacoin-pro/core/chain/state"
+	"github.com/mediacoin-pro/core/common/hex"
+	"github.com/mediacoin-pro/core/crypto"
+)
+
+type transactionJSON struct {
+	TxID         hex.Uint64        `json:"id"`             //
+	TxHash       hex.Bytes         `json:"hash"`           //
+	BlockNum     uint64            `json:"block_num"`      //
+	BlockIdx     int               `json:"block_idx"`      //
+	BlockTs      int64             `json:"block_ts"`       //
+	TxSeq        string            `json:"seq"`            //
+	Type         int               `json:"type"`           // tx type
+	TypeStr      string            `json:"stype"`          // tx type as string
+	Version      int               `json:"version"`        // tx version
+	Network      int               `json:"network"`        //
+	ChainID      uint64            `json:"chain"`          //
+	Nonce        uint64            `json:"nonce"`          //
+	Sender       *crypto.PublicKey `json:"sender"`         // tx sender
+	SenderAddr   string            `json:"sender_address"` // tx sender address
+	SenderNick   string            `json:"sender_nick"`    // tx sender nickname (can be empty)
+	ObjRaw       hex.Bytes         `json:"raw_data"`       // encoded tx-data
+	Obj          ITransaction      `json:"obj"`            // unserialized data
+	Sig          hex.Bytes         `json:"sig"`            //
+	StateUpdates state.Values      `json:"state"`          //
+}
+
+func (tx *Transaction) MarshalJSON() ([]byte, error) {
+	if tx == nil {
+		return json.Marshal(nil)
+	}
+	nick, _ := tx.SenderNick()
+	return json.Marshal(&transactionJSON{
+		Type:         tx.Type,
+		TypeStr:      tx.StrType(),
+		Version:      tx.Version,
+		Network:      tx.Network,
+		ChainID:      tx.ChainID,
+		Nonce:        tx.Nonce,
+		Sender:       tx.Sender,
+		SenderAddr:   tx.SenderAddressStr(),
+		SenderNick:   nick,
+		ObjRaw:       tx.Data,
+		Obj:          tx.TxObject(),
+		TxID:         hex.Uint64(tx.ID()),
+		TxHash:       hex.Bytes(tx.Hash()),
+		Sig:          hex.Bytes(tx.Sig),
+		BlockNum:     tx.BlockNum(),
+		BlockIdx:     tx.BlockIdx(),
+		BlockTs:      tx.BlockTs(),
+		TxSeq:        "0x" + hex.EncodeUint(tx.Seq()),
+		StateUpdates: tx.StateUpdates,
+	})
+}
