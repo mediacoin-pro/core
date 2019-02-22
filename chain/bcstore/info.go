eccaba64e4640a23e2b51ba16c8e3ee01bd0804f
mdcman <mediacoin@mediacoin.pro>
2019-02-22 16:08:01 +0300

    modify blockchain-info

--- chain/bcstore/info.go
+++ chain/bcstore/info.go
@@ -3,18 +3,16 @@ package bcstore
 import "github.com/mediacoin-pro/core/chain"
 
 type Info struct {
-	Version   string             `json:"ver"`        //
+	Network   int                `json:"network"`    //
 	ChainID   uint64             `json:"chain"`      //
 	Stat      *Statistic         `json:"stat"`       //
 	LastBlock *chain.BlockHeader `json:"last_block"` //
-	//Mempool   mempool.Info            `json:"mempool"`    //
 }
 
 func (s *ChainStorage) Info() (inf Info, err error) {
-	inf.Version = "" //, s.Cfg.Version
+	inf.Network = s.Cfg.NetworkID
 	inf.ChainID = s.Cfg.ChainID
 	inf.Stat = s.Totals()
 	inf.LastBlock = s.LastBlockHeader()
-	//inf.Mempool = s.Mempool.Info()
 	return
 }
